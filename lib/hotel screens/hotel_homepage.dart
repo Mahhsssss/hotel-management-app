54aca730a4ae826bc974a317fe543a634ee04542
Maha <mahaarshadsheikh@gmail.com>
2026-01-26 23:14:11 +0530

    hotel homepage completed w/o any pictures, need buttonpress events

--- lib/hotel screens/hotel_homepage.dart
+++ lib/hotel screens/hotel_homepage.dart
@@ -10,11 +10,139 @@ class HotelHomepage extends StatefulWidget {
 }
 
 class _HotelHomepageState extends State<HotelHomepage> {
+  final ScrollController _mainScrollController = ScrollController();
+
+  int _currentCardIndex = 0;
+
+  static Widget _buildCard(String title, String sub, String image) {
+    return GFCard(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(0),
+      showOverlayImage: true,
+      imageOverlay: AssetImage(image),
+      boxFit: BoxFit.cover,
+      title: GFListTile(
+        title: Text(
+          title,
+          style: AppWidget.headingcustomtext(Colors.white, 15),
+        ),
+        subTitle: Text(sub, style: AppWidget.smalltext(Colors.white, 12)),
+      ),
+    );
+  }
+
+  static Widget _buildListItem(
+    String name,
+    String location,
+    String price,
+    String ratings,
+    String image,
+  ) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 5,
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Padding(
+                padding: EdgeInsetsGeometry.all(5),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(image, fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            const SizedBox(width: 5),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: AppWidget.headingcustomtext(Colors.black, 15),
+                    ),
+                    Text(location, style: AppWidget.bodytext(Colors.black, 12)),
+                    SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          price,
+                          style: AppWidget.bodytext(Colors.black, 10),
+                        ),
+                        Text(
+                          ratings,
+                          style: AppWidget.bodytext(Colors.black, 10),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  // final List<Widget> cards
+
+  final List<Map<String, String>> hotels = [
+    {
+      "name": 'Hotel Name',
+      "location": 'Location',
+      "price": '100.00',
+      "rating": '4.5/5',
+      "image": "assets/images/placeholder.jpg",
+    },
+
+    {
+      "name": 'Hotel Name',
+      "location": 'Location',
+      "price": '100.00',
+      "rating": '4.5/5',
+      "image": "assets/images/placeholder.jpg",
+    },
+
+    {
+      "name": 'Hotel Name',
+      "location": 'Location',
+      "price": '100.00',
+      "rating": '4.5/5',
+      "image": "assets/images/placeholder.jpg",
+    },
+
+    {
+      "name": 'Hotel Name',
+      "location": 'Location',
+      "price": '100.00',
+      "rating": '4.5/5',
+      "image": "assets/images/placeholder.jpg",
+    },
+
+    {
+      "name": 'Hotel Name',
+      "location": 'Location',
+      "price": '100.00',
+      "rating": '4.5/5',
+      "image": "assets/images/placeholder.jpg",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFE8F4EA),
-      body: Container(
+      body: SingleChildScrollView(
+        controller: _mainScrollController,
+        physics: BouncingScrollPhysics(),
         child: Column(
           children: [
             Stack(
@@ -25,8 +153,8 @@ class _HotelHomepageState extends State<HotelHomepage> {
                     bottomRight: Radius.circular(20),
                   ),
                   child: Container(
-                    padding: EdgeInsets.only(top: 50, left: 30, right: 30),
-                    height: 250,
+                    padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+                    height: 220,
                     width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
                       image: DecorationImage(
@@ -45,10 +173,7 @@ class _HotelHomepageState extends State<HotelHomepage> {
                       children: [
                         Row(
                           children: [
-                            Icon(
-                              Icons.location_on_outlined,
-                              color: Colors.white,
-                            ),
+                            Icon(Icons.location_on_sharp, color: Colors.white),
                             SizedBox(width: 4),
                             Text(
                               "Mumbai, India",
@@ -63,11 +188,11 @@ class _HotelHomepageState extends State<HotelHomepage> {
                         ),
                         SizedBox(height: 10),
                         Container(
-                          margin: EdgeInsets.only(top: 20, right: 5, left: 5),
+                          margin: EdgeInsets.only(top: 15, right: 5, left: 5),
                           width: MediaQuery.of(context).size.width,
                           decoration: BoxDecoration(
-                            color: const Color.fromARGB(117, 255, 255, 255),
-                            borderRadius: BorderRadius.circular(25),
+                            color: const Color.fromARGB(130, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(20),
                           ),
                           child: TextField(
                             textAlignVertical: TextAlignVertical.center,
@@ -88,19 +213,136 @@ class _HotelHomepageState extends State<HotelHomepage> {
                 ),
               ],
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  "Featured Retreats ",
-                  style: AppWidget.headingcustomtext(Colors.black, 18),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Featured Retreats ",
+                    style: AppWidget.headingcustomtext(Colors.black, 20),
+                  ),
+                  GFButton(
+                    onPressed: () {
+                      print("Button tapped");
+                    },
+                    text: "See all",
+                    color: const Color.fromARGB(255, 50, 109, 48),
+                    type: GFButtonType.transparent,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 220,
+              width: MediaQuery.of(context).size.width,
+              child: GFCarousel(
+                scrollPhysics: const AlwaysScrollableScrollPhysics(),
+                height: 190,
+                items: [
+                  _buildCard(
+                    "Hotel name",
+                    "description",
+                    "assets/images/placeholder.jpg",
+                  ),
+                  _buildCard(
+                    "Hotel name",
+                    "description",
+                    "assets/images/placeholder.jpg",
+                  ),
+                  _buildCard(
+                    "Hotel name",
+                    "description",
+                    "assets/images/placeholder.jpg",
+                  ),
+                ],
+                viewportFraction: 0.8,
+                onPageChanged: (i) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    if (mounted) {
+                      setState(() {
+                        _currentCardIndex = i;
+                      });
+                    }
+                  });
+                },
+                enlargeMainPage: true,
+                enableInfiniteScroll: true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Recommended",
+                    style: AppWidget.headingcustomtext(Colors.black, 20),
+                  ),
+                  GFButton(
+                    onPressed: () {
+                      print("Button tapped");
+                    },
+                    text: "See all",
+                    color: const Color.fromARGB(255, 50, 109, 48),
+                    type: GFButtonType.transparent,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 250,
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.separated(
+                  padding: EdgeInsets.zero,
+                  itemCount: 5,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(height: 5),
+                  itemBuilder: (BuildContext context, int index) {
+                    final hotel = hotels[index];
+
+                    return _buildListItem(
+                      hotel["name"]!,
+                      hotel["location"]!,
+                      hotel["price"]!,
+                      hotel["rating"]!,
+                      hotel["image"]!,
+                    );
+                  },
                 ),
-                GFButton(
-                  onPressed: () {},
-                  text: "See all",
-                  type: GFButtonType.transparent,
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 50,
+              width: 275,
+              child: Container(
+                // margin: EdgeInsets.only(left: 45, right: 45),
+                // padding: EdgeInsets.only(),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-              ],
+                child: ElevatedButton(
+                  onPressed: () {
+                    /* Will navigate to booking screen/ filtering screen*/
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    visualDensity: VisualDensity(horizontal: 3, vertical: 3),
+                    shadowColor: const Color.fromARGB(255, 28, 65, 29),
+                    backgroundColor: const Color.fromARGB(255, 125, 175, 68),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    'Start Booking now',
+                    style: AppWidget.bodytext(Colors.black, 15),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
