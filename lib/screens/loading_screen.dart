e3693c63b6ea47954ce5ad9589605583e559bf3a
Mabel <mabeldsouza2007@gmail.com>
2026-02-05 18:11:44 +0530

    added filtering screen

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SizedBox(
+          width: 250,
+          height: 250,
+          child: Lottie.asset(
+            'assets/lottie/calendar_loading.json',
+            repeat: true,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+}
